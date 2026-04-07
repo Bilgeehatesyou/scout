4af8516830be36c77adcccfff04fb7e34521c1a5
Bilgeehatesyou <bilgeehatesyou@gmail.com>
2026-04-07 19:53:59 +0800

    folder structure

--- js/events_page.js
+++ js/events_page.js
@@ -11,7 +11,7 @@
   let pastShown     = 0;
 
   /* ── Fetch JSON ── */
-fetch('./json/events.json')
+fetch('/json/events.json')
   .then(r => {
     if (!r.ok) {
       throw new Error('JSON олдсонгүй: ' + r.status);
